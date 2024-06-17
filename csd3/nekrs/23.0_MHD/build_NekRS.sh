6621d7113734609b91b1412c3087a9a14b8fdbd0
Rupert Eardley <rupert.eardley@ukaea.uk>
2024-06-17 14:44:59 +0100

    Add NekRS MHD build scripts

---
+++ csd3/nekrs/23.0_MHD/build_NekRS.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# set NekRS version
+version_major=23
+version_minor=0
+
+# set installation location
+
+# MY_RDS=       # set $MY_RDS here if required
+DIR_NAME=MHD_${version_major}.${version_minor}
+NEKRS_GENERAL_DIR=${MY_RDS}/NekRS
+INSTALL_DIR=${NEKRS_GENERAL_DIR}/${DIR_NAME}
+
+# Direct install script to the nekrs_mhd repo; set as required
+NEKRS_MHD_DIR=${NEKRS_GENERAL_DIR}/nekrs_mhd
+
+## Don't modify this script below this line
+
+# Write NekRS profile
+
+PROFILE_NAME=nekrs_MHD_${version_major}-${version_minor}_profile
+
+echo "module purge" > $HOME/.$PROFILE_NAME
+echo "module load rhel8/default-amp" >> $HOME/.$PROFILE_NAME
+echo "export CC=mpicc" >> $HOME/.$PROFILE_NAME
+echo "export CXX=mpic++" >> $HOME/.$PROFILE_NAME
+echo "export FC=mpif77" >> $HOME/.$PROFILE_NAME
+echo "export NEKRS_HOME=$INSTALL_DIR/nekRS" >> $HOME/.$PROFILE_NAME
+echo "export NEKRS_TOOLS=$INSTALL_DIR/build/3rd_party/nek5000/bin" >> $HOME/.$PROFILE_NAME
+
+echo "export PATH=\${NEKRS_HOME}/bin:\${PATH}" >> $HOME/.$PROFILE_NAME
+echo "export PATH=\${NEKRS_TOOLS}:\${PATH}" >> $HOME/.$PROFILE_NAME
+
+echo "export NRS_RUN=$NEKRS_GENERAL_DIR/user_problems" >> $HOME/.$PROFILE_NAME
+echo "alias nrs='cd \${NRS_RUN} && ls'" >> $HOME/.$PROFILE_NAME
+
+# Load NekRS profile
+
+source ~/.$PROFILE_NAME
+
+# make directory
+mkdir -p $INSTALL_DIR
+
+# store details of build environment
+
+mkdir $INSTALL_DIR/setup
+cd $INSTALL_DIR/setup
+env > log.env
+cp ~/.bashrc log.bashrc
+cp ~/.bash_profile log.bash_profile
+cp ~/.$PROFILE_NAME log.$PROFILE_NAME
+
+# get NekRS source code
+
+cd $INSTALL_DIR
+git clone https://github.com/Nek5000/nekRS.git
+cd $INSTALL_DIR/nekRS
+git checkout v$version_major.$version_minor
+cd $INSTALL_DIR
+mv nekRS source
+
+# get NekRS MHD repo
+
+
+cp $NEKRS_MHD_DIR/bdry.f source/3rd_party/nek5000/core/
+cp -r $NEKRS_MHD_DIR/src/* source/src/
+cp -r $NEKRS_MHD_DIR/MHD source/examples/
+
+# build NekRS
+
+cd $INSTALL_DIR/source
+
+echo "++++++++++++++++++++++"
+echo "+++ Building NekRS +++"
+echo "++++++++++++++++++++++"
+
+# remove user input requirements
+sed -i s/'read -p "Press ENTER to continue or ctrl-c to cancel"'/''/g nrsconfig
+
+# run config
+CC=$CC CXX=$CXX FC=$FC ./nrsconfig -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR/nekRS 2>&1 | tee $INSTALL_DIR/setup/log.nrsconfig
+
+echo "++++++++++++++++++++++"
+echo "++++ NekRS Built +++++"
+echo "++++++++++++++++++++++"
