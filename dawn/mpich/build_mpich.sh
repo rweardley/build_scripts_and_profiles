6ef30fab79703a96c7046c34895f41283216ccb8
Rupert Eardley <rupert.eardley@ukaea.uk>
2024-09-26 14:50:35 +0100

    Add latest NekRS Dawn MPICH build scripts

--- dawn/mpich/build_mpich.sh
+++ dawn/mpich/build_mpich.sh
@@ -10,6 +10,8 @@ MPICH_DIR=$INSTALL_DIR/mpich-install
 HELP2MAN_DIR=$INSTALL_DIR/help2man
 LIBTOOL_DIR=$INSTALL_DIR/libtool
 DOWNLOAD_DIR=$INSTALL_DIR/downloads
+MPICH_MODULE_DIR=$HOME/privatemodules
+MPICH_MODULE=$MPICH_MODULE_DIR/mpich_custom
 
 # set up environment
 
@@ -64,3 +66,24 @@ export ACLOCAL_PATH=$LIBTOOL_DIR/share/aclocal:$ACLOCAL_PATH
 ./configure --prefix=$MPICH_DIR
 make -j24
 make -j24 install
+
+# create custom modulefile
+
+mkdir -p $MPICH_MODULE_DIR
+echo "#%Module1.0" > $MPICH_MODULE
+echo "module-whatis {Custom MPICH module for multi-node NekRS on Dawn}" >> $MPICH_MODULE
+echo "proc ModulesHelp { } { puts stderr {Name   : Custom MPICH} }" >> $MPICH_MODULE
+echo "prepend-path PATH {$LIBTOOL_DIR/bin}" >> $MPICH_MODULE
+echo "prepend-path ACLOCAL_PATH {$LIBTOOL_DIR/share/aclocal}" >> $MPICH_MODULE
+echo "prepend-path CPATH {$MPICH_DIR/include}" >> $MPICH_MODULE
+echo "setenv I_MPI_ROOT {$MPICH_DIR}" >> $MPICH_MODULE
+echo "prepend-path LIBRARY_PATH {$MPICH_DIR/lib}" >> $MPICH_MODULE
+echo "prepend-path PKG_CONFIG_PATH {$MPICH_DIR/lib/pkgconfig}" >> $MPICH_MODULE
+echo "prepend-path LD_LIBRARY_PATH {$MPICH_DIR/lib}" >> $MPICH_MODULE
+echo "prepend-path PATH {$MPICH_DIR/bin}" >> $MPICH_MODULE
+echo "setenv I_MPI_CC {icx}" >> $MPICH_MODULE
+echo "setenv I_MPI_CXX {icpx}" >> $MPICH_MODULE
+echo "setenv I_MPI_F77 {ifx}" >> $MPICH_MODULE
+echo "setenv I_MPI_F90 {ifx}" >> $MPICH_MODULE
+echo "setenv I_MPI_FC {ifx}" >> $MPICH_MODULE
+echo "append-path MANPATH {}" >> $MPICH_MODULE # might not be needed
