6da1d21d7f8178cf05faa3e8c4023fbf870b0788
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2025-07-11 11:00:19 +0100

    Update Cardinal build script and add test for build with NekRS v24

--- desktop/nekrs/cardinal/buildCardinal.sh
+++ desktop/nekrs/cardinal/buildCardinal.sh
@@ -17,11 +17,11 @@ export ENABLE_OPENMC=false
 export ENABLE_DAGMC=false
 
 # Set number of cores for compilation
-export MOOSE_JOBS=32
+export MOOSE_JOBS=12
 
 # Set installation directory
 # Cardinal will be installed in a `cardinal` directory inside this directory
-INSTALL_DIR="${PWD}"
+INSTALL_DIR="${HOME}/NekRS"
 
 ### Don't modify anything below this ###
 
@@ -31,8 +31,8 @@ echo 'PS1="\e[1;31m(Cardinal)\e[0m "$PS1' > $HOME/.cardinal_profile
 echo "export CC=mpicc" >> $HOME/.cardinal_profile
 echo "export CXX=mpicxx" >> $HOME/.cardinal_profile
 echo "export FC=mpif90" >> $HOME/.cardinal_profile
-echo "export PATH=\$PATH:/usr/local/cuda-11.8/bin" >> $HOME/.cardinal_profile	# desktop only
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda-11.8/lib64" >> $HOME/.cardinal_profile	# desktop only
+echo "export PATH=\$PATH:/usr/local/cuda-12/bin" >> $HOME/.cardinal_profile	# desktop only
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda-12/lib64" >> $HOME/.cardinal_profile	# desktop only
 echo "export CARDINAL_DIR=${INSTALL_DIR}/cardinal" >> $HOME/.cardinal_profile
 echo "export PATH=\$PATH:\$CARDINAL_DIR" >> $HOME/.cardinal_profile
 if $ENABLE_NEK ; then
