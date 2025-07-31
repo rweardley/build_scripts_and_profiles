d0950ff6dfe691f9f69917016c5fc5a3e2e486bc
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2025-07-31 11:08:18 +0100

    Tidy rebuild script

--- desktop/nekrs/aurora-mhd/rebuild_NekRS.sh
+++ desktop/nekrs/aurora-mhd/rebuild_NekRS.sh
@@ -12,45 +12,9 @@ INSTALL_DIR=${NEKRS_GENERAL_DIR}/${DIR_NAME}
 
 PROFILE_NAME=nekrs_aurora-mhd_profile
 
-#echo "export CC=mpicc" >> $HOME/.$PROFILE_NAME
-#echo "export CXX=mpic++" >> $HOME/.$PROFILE_NAME
-#echo "export FC=mpif77" >> $HOME/.$PROFILE_NAME
-#echo "export PATH=\${PATH}:/usr/local/cuda-12/bin" >> $HOME/.$PROFILE_NAME
-#echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/usr/local/cuda-12/lib64" >> $HOME/.$PROFILE_NAME
-#echo "export NEKRS_HOME=$INSTALL_DIR/nekRS" >> $HOME/.$PROFILE_NAME
-
-#echo "export PATH=\${NEKRS_HOME}/bin:\${PATH}" >> $HOME/.$PROFILE_NAME
-
-#echo "export NRS_RUN=$NEKRS_GENERAL_DIR/user_problems" >> $HOME/.$PROFILE_NAME
-#echo "alias nrs='cd \${NRS_RUN} && ls'" >> $HOME/.$PROFILE_NAME
-#echo "alias nekrun='mpirun --mca osc ucx -np 1 nekrs --setup'" >> $HOME/.$PROFILE_NAME
-
 # Load NekRS profile
 
 source ~/.$PROFILE_NAME
-
-# make directory
-#mkdir -p $INSTALL_DIR
-
-# store details of build environment
-
-#mkdir $INSTALL_DIR/setup
-#cd $INSTALL_DIR/setup
-#env > log.env
-#cp ~/.bashrc log.bashrc
-#cp ~/.bash_profile log.bash_profile
-#cp ~/.$PROFILE_NAME log.$PROFILE_NAME
-
-# get NekRS source code
-
-#cd $INSTALL_DIR
-#git clone git@github.com:guo-yichen/nekRS_MHD_code.git
-#cd $INSTALL_DIR/nekRS_MHD_code
-#git checkout aurora-mhd
-#cd $INSTALL_DIR
-#mv nekRS_MHD_code source
-
-# build NekRS
 
 cd $INSTALL_DIR/source
 
