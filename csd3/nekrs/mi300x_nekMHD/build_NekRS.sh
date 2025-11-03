1e89dfb93eed935a827a8e2b1b0e338e2e3f8a4b
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2025-11-03 09:47:39 +0000

    Add MI300X nekMHD build script

---
+++ csd3/nekrs/mi300x_nekMHD/build_NekRS.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# get date
+DATE_TODAY=$(date +'%y%m%d')
+
+# set installation location
+
+# MY_RDS=       # set $MY_RDS here if required
+NODE_TYPE=QPX
+# NODE_TYPE=SPX
+# NODE_TYPE=CPX
+DIR_NAME=nekMHD_${DATE_TODAY}_${NODE_TYPE}
+NEKRS_GENERAL_DIR=${MY_RDS}/NekRS/MI300X
+INSTALL_DIR=${NEKRS_GENERAL_DIR}/${DIR_NAME}
+
+# Direct install script to the nekrs_mhd repo; set as required
+# shouldn't change the name of the directory itself
+NEKRS_MHD_DIR=${HOME}/nekMHD
+
+## Don't modify this script below this line
+
+ORIGIN_DIR=$PWD
+
+# Write NekRS profile
+
+PROFILE_NAME=nekrs_mi300x_nekMHD_${DATE_TODAY}_${NODE_TYPE}_profile
+
+echo "module purge" > $HOME/.$PROFILE_NAME
+echo "module load rhel9/default-amdgpu" >> $HOME/.$PROFILE_NAME
+echo "module load rocm" >> $HOME/.$PROFILE_NAME
+echo "export CC=mpicc" >> $HOME/.$PROFILE_NAME
+echo "export CXX=mpicxx" >> $HOME/.$PROFILE_NAME
+echo "export FC=mpifc" >> $HOME/.$PROFILE_NAME
+echo "export I_MPI_CC=hipcc" >> $HOME/.$PROFILE_NAME
+echo "export I_MPI_CXX=hipcc" >> $HOME/.$PROFILE_NAME
+echo "export I_MPI_FC=amdflang-classic" >> $HOME/.$PROFILE_NAME
+echo "export HIPCXXFLAGS=\"-std=c++17\"" >> $HOME/.$PROFILE_NAME # fix for rocm 7.0 dropping c++14 support
+echo "export CMAKE_PREFIX_PATH=/opt/rocm:\$CMAKE_PREFIX_PATH" >> $HOME/.$PROFILE_NAME
+echo "export ROCM_HOME=/opt/rocm" >> $HOME/.$PROFILE_NAME
+echo "unset I_MPI_PMI_LIBRARY" >> $HOME/.$PROFILE_NAME
+echo "export LD_LIBRARY_PATH=/opt/rocm/lib/llvm/lib:\${LD_LIBRARY_PATH}" >> $HOME/.$PROFILE_NAME # fix for missing libpgmath.so
+echo "export NEKRS_HOME=$INSTALL_DIR/nekRS" >> $HOME/.$PROFILE_NAME
+echo "export NEKRS_TOOLS=$INSTALL_DIR/build/3rd_party/nek5000/bin" >> $HOME/.$PROFILE_NAME
+
+echo "export PATH=\${NEKRS_HOME}/bin:\${PATH}" >> $HOME/.$PROFILE_NAME
+echo "export PATH=\${NEKRS_TOOLS}:\${PATH}" >> $HOME/.$PROFILE_NAME
+
+echo "export NRS_RUN=$NEKRS_GENERAL_DIR/user_problems" >> $HOME/.$PROFILE_NAME
+echo "alias nrs='cd \${NRS_RUN} && ls'" >> $HOME/.$PROFILE_NAME
+
+# Load NekRS profile
+
+source ~/.$PROFILE_NAME
+
+# make directory
+mkdir -p $INSTALL_DIR
+
+# store details of build environment
+
+mkdir $INSTALL_DIR/setup
+cd $INSTALL_DIR/setup
+module list > log.module_list
+env > log.env
+rocminfo > log.rocminfo
+rocm-smi > log.rocm-smi
+rocm-smi --showtopo > log.rocm-smi-topo
+
+cp ~/.bashrc log.bashrc
+cp ~/.bash_profile log.bash_profile
+cp ~/.$PROFILE_NAME log.$PROFILE_NAME
+
+# get NekRS source code
+
+cd $INSTALL_DIR
+cp -r $NEKRS_MHD_DIR source
+
+# build NekRS
+
+cd $INSTALL_DIR/source
+
+echo "++++++++++++++++++++++"
+echo "+++ Building NekRS +++"
+echo "++++++++++++++++++++++"
+
+# remove user input requirements
+sed -i s/'read -rsn1 key'/''/g build.sh
+
+# run config
+./build.sh -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/nekRS -DENABLE_HYPRE_GPU=on -DNEKRS_Fortran_FLAGS="-fuse-ld=bfd" 2>&1 | tee $INSTALL_DIR/setup/log.build
+
+echo "++++++++++++++++++++++"
+echo "++++ NekRS Built +++++"
+echo "++++++++++++++++++++++"
