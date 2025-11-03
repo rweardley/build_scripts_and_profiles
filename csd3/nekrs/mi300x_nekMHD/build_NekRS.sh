f57e54b195bfc159e2ca9bfa7e1c1190fca8f457
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2025-11-03 10:16:48 +0000

    Add syncwarp patch

--- csd3/nekrs/mi300x_nekMHD/build_NekRS.sh
+++ csd3/nekrs/mi300x_nekMHD/build_NekRS.sh
@@ -73,6 +73,9 @@ cp ~/.$PROFILE_NAME log.$PROFILE_NAME
 
 cd $INSTALL_DIR
 cp -r $NEKRS_MHD_DIR source
+cd source
+git apply $ORIGIN_DIR/syncwarp.patch # syncwarp patch
+cd $INSTALL_DIR
 
 # build NekRS
 
