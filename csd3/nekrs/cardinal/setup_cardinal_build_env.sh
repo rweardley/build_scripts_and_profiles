96a840d116144a5f1f6845e5459b658d67f19c36
Kyle Damm <kyle.anthony.damm@gmail.com>
2025-11-24 16:12:36 +0000

    Add bash script to automate configuration of a Python environment for building Cardinal locally.

---
+++ csd3/nekrs/cardinal/setup_cardinal_build_env.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+ENV_DIR="cardinal-py-env"
+
+# List packages here
+PACKAGES=(
+    setuptools
+    jinja2
+    packaging
+    pyyaml
+)
+
+# Setup environment
+if [ ! -d "$ENV_DIR" ]; then
+  echo "Creating virtual environment in '$ENV_DIR'..."
+  python3 -m venv "$ENV_DIR"
+
+  echo "Installing packages..."
+  "$ENV_DIR/bin/pip3" install "${PACKAGES[@]}"
+else
+  echo "Using existing virtual environment '$ENV_DIR'."
+fi
+
+# Activate environment
+# (only works when you run: source setup_env.sh)
+source "$ENV_DIR/bin/activate"
+
+echo "Environment active: $(python -c 'import sys; print(sys.executable)')"
