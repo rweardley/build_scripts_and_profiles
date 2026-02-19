01e792bb47dd2d1fb90ede8597980ac0114893b2
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-02-19 10:03:27 +0000

    Add build script for NekRS MHD v26

---
+++ desktop/nekrs/nekMHD-v26/build_NekRS.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# set installation location
+
+DIR_NAME=nekMHD-v26
+NEKRS_GENERAL_DIR=${HOME}/NekRS
+INSTALL_DIR=${NEKRS_GENERAL_DIR}/${DIR_NAME}
+
+# set path to CUDA
+CUDA_DIR=$HOME/Programs/cuda/cuda-12.2
+
+## Don't modify this script below this line
+
+# Write NekRS profile
+
+PROFILE_NAME=nekrs_nekMHD-v26_profile
+
+echo "export CC=mpicc" > $HOME/.$PROFILE_NAME
+echo "export CXX=mpic++" >> $HOME/.$PROFILE_NAME
+echo "export FC=mpif77" >> $HOME/.$PROFILE_NAME
+
+echo "export CUDA_DIR=$CUDA_DIR" >> $HOME/.$PROFILE_NAME
+echo "export PATH=\${CUDA_DIR}/bin:\${PATH}" >> $HOME/.$PROFILE_NAME
+echo "export CPATH=\${CUDA_DIR}/include:\${CPATH}" >> $HOME/.$PROFILE_NAME
+echo "export LIBRARY_PATH=\${CUDA_DIR}/lib64:\${LIBRARY_PATH}" >> $HOME/.$PROFILE_NAME
+echo "export LD_LIBRARY_PATH=\${CUDA_DIR}/lib64:\${LD_LIBRARY_PATH}" >> $HOME/.$PROFILE_NAME
+echo "export CUDAToolkit_ROOT=\${CUDA_DIR}" >> $HOME/.$PROFILE_NAME
+echo "export CMAKE_CUDA_TOOLKIT_ROOT_DIR=\${CUDA_DIR}" >> $HOME/.$PROFILE_NAME
+echo "export NVCC=\${CUDA_DIR}/bin/nvcc" >> $HOME/.$PROFILE_NAME
+echo "export CUDACXX=\${CUDA_DIR}/bin/nvcc" >> $HOME/.$PROFILE_NAME
+echo "export CMAKE_CUDA_COMPILER=\${CUDA_DIR}/bin/nvcc" >> $HOME/.$PROFILE_NAME
+
+echo "export NEKRS_HOME=$INSTALL_DIR/nekRS" >> $HOME/.$PROFILE_NAME
+echo "export NRS_RUN=$NEKRS_GENERAL_DIR/user_problems" >> $HOME/.$PROFILE_NAME
+echo "export PATH=\${NEKRS_HOME}/bin:\${PATH}" >> $HOME/.$PROFILE_NAME
+
+echo "alias nrs='cd \${NRS_RUN} && ls'" >> $HOME/.$PROFILE_NAME
+echo "alias nekrun='mpirun --mca osc ucx -np 1 nekrs --setup'" >> $HOME/.$PROFILE_NAME
+
+# Load NekRS profile
+
+source ~/.$PROFILE_NAME
+
+# check we are using the expected CUDA toolkit
+echo "CUDA nvcc:" "$(command -v nvcc)"
+nvcc --version
+
+# make directory
+mkdir -p $INSTALL_DIR
+
+# store details of build environment
+
+mkdir $INSTALL_DIR/setup
+cd $INSTALL_DIR/setup
+env > log.env
+cp ~/.bashrc log.bashrc
+cp ~/.bash_profile log.bash_profile
+cp ~/.$PROFILE_NAME log.$PROFILE_NAME
+
+# get NekRS source code
+
+cd $INSTALL_DIR
+git clone git@github.com:misunmin/nekMHD.git
+cd $INSTALL_DIR/nekMHD
+git checkout v26_mhd
+cd $INSTALL_DIR
+mv nekMHD source
+
+# build NekRS
+
+cd $INSTALL_DIR/source
+
+echo "++++++++++++++++++++++"
+echo "+++ Building NekRS +++"
+echo "++++++++++++++++++++++"
+
+# remove user input requirements
+sed -i s/'read -rsn1 key'/''/g build.sh
+
+# run config
+./build.sh -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR/nekRS 2>&1 | tee $INSTALL_DIR/setup/log.build
+
+echo "++++++++++++++++++++++"
+echo "++++ NekRS Built +++++"
+echo "++++++++++++++++++++++"
