c332c99a46374ac4a22d027cde5f4bc56e0ae6c4
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-02-19 10:10:14 +0000

    Add v26 NekRS MHD build script for CSD3

---
+++ csd3/nekrs/nekMHD-v26/install_cuda_12.2.0.sh
@@ -0,0 +1,17 @@
+# Download the CUDA 12.2 user-space installer
+wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers\
+/cuda_12.2.0_535.54.03_linux.run
+
+# Make the installer executable
+chmod +x cuda_12.2.0_535.54.03_linux.run
+
+# Install the CUDA 12.2 toolkit locally without drivers or sudo
+./cuda_12.2.0_535.54.03_linux.run \
+  --silent \
+  --toolkit \
+  --toolkitpath=$MY_RDS/cuda/cuda-12.2 \
+  --no-drm \
+  --override
+
+# Clean up
+rm cuda_12.2.0_535.54.03_linux.run
