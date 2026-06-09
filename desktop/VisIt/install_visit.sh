864de1b83a7b3aaf16992a44a77f6902afbdb826
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-06-09 16:18:09 +0100

    Add VisIt scripts

---
+++ desktop/VisIt/install_visit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VERSION_MAJOR=3
+VERSION_MINOR=5
+VERSION_PATCH=0
+
+INSTALL_DIR=$HOME/Programs/visit
+
+PLATFORM=linux-x86_64-ubuntu22
+
+VERSION_PERIOD=${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
+VERSION_USCORE=${VERSION_MAJOR}_${VERSION_MINOR}_${VERSION_PATCH}
+
+VISIT_RELEASE=https://github.com/visit-dav/visit/releases/download/v${VERSION_PERIOD}
+
+mkdir temp
+cd temp
+wget ${VISIT_RELEASE}/visit-install${VERSION_USCORE}
+wget ${VISIT_RELEASE}/visit${VERSION_USCORE}.${PLATFORM}.tar.gz
+
+chmod +x visit-install${VERSION_USCORE}
+
+./visit-install${VERSION_USCORE} $VERSION_PERIOD $PLATFORM $INSTALL_DIR << EOF 2>&1 | tee ../log.install
+1
+EOF
+
+cd ..
+rm -r temp
