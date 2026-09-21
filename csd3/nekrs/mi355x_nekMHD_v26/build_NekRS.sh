44cdad703e883dd179790d2d3912c2f407788b92
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-09-21 16:13:39 +0100

    Add nekMHD v26 mi355x scripts

---
+++ csd3/nekrs/mi355x_nekMHD_v26/build_NekRS.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# get date
+DATE_TODAY=$(date +'%y%m%d')
+
+# set installation location
+
+# MY_RDS=       # set $MY_RDS here if required
+DIR_NAME=nekMHD-v26_${DATE_TODAY}
+NEKRS_GENERAL_DIR=${MY_RDS}/NekRS/MI355X
+INSTALL_DIR=${NEKRS_GENERAL_DIR}/${DIR_NAME}
+
+# Direct install script to the nekrs_mhd repo; set as required
+# shouldn't change the name of the directory itself
+NEKRS_MHD_DIR=${HOME}/nekMHD-v26
+
+## Don't modify this script below this line
+
+ORIGIN_DIR=$PWD
+
+# Write NekRS profile
+
+PROFILE_NAME=nekrs_mi355x_nekMHD_v26_${DATE_TODAY}_profile
+
+echo "module purge" > $HOME/.$PROFILE_NAME
+echo "module load rhel9/mi355x/base" >> $HOME/.$PROFILE_NAME
+echo "module load rocm/7.14" >> $HOME/.$PROFILE_NAME
+echo "module load openmpi/5.0.10/" >> $HOME/.$PROFILE_NAME
+echo "export CC=mpicc" >> $HOME/.$PROFILE_NAME
+echo "export CXX=mpicxx" >> $HOME/.$PROFILE_NAME
+echo "export FC=mpifort" >> $HOME/.$PROFILE_NAME
+echo "export HIPCXXFLAGS=\"-std=c++17\"" >> $HOME/.$PROFILE_NAME # fix for rocm 7.0 dropping c++14 support
+echo "export NEKRS_HOME=$INSTALL_DIR/nekRS" >> $HOME/.$PROFILE_NAME
+echo "export NEKRS_TOOLS=$INSTALL_DIR/build/3rd_party/nek5000/bin" >> $HOME/.$PROFILE_NAME
+
+echo "export PATH=\${NEKRS_HOME}/bin:\${PATH}" >> $HOME/.$PROFILE_NAME
+echo "export PATH=\${NEKRS_TOOLS}:\${PATH}" >> $HOME/.$PROFILE_NAME
+
+echo "export NRS_RUN=$NEKRS_GENERAL_DIR/user_problems" >> $HOME/.$PROFILE_NAME
+echo "alias nrs='cd \${NRS_RUN} && ls'" >> $HOME/.$PROFILE_NAME
+
+# Load NekRS profile
+
+source ~/.$PROFILE_NAME
+
+# make directory
+mkdir -p $INSTALL_DIR
+
+# store details of build environment
+
+mkdir $INSTALL_DIR/setup
+cd $INSTALL_DIR/setup
+module list > log.module_list
+env > log.env
+rocminfo > log.rocminfo
+rocm-smi > log.rocm-smi
+rocm-smi --showtopo > log.rocm-smi-topo
+
+cp ~/.bashrc log.bashrc
+cp ~/.bash_profile log.bash_profile
+cp ~/.$PROFILE_NAME log.$PROFILE_NAME
+
+# get NekRS source code
+
+cd $INSTALL_DIR
+cp -r $NEKRS_MHD_DIR source
+cd $INSTALL_DIR/source
+git apply $ORIGIN_DIR/syncwarp.patch # syncwarp patch
+git apply $ORIGIN_DIR/hypre_cmake.patch # hypre cmake patch
+
+# build NekRS
+
+cd $INSTALL_DIR/source
+
+echo "++++++++++++++++++++++"
+echo "+++ Building NekRS +++"
+echo "++++++++++++++++++++++"
+
+# remove user input requirements
+sed -i s/'read -rsn1 key'/''/g build.sh
+
+# run config
+./build.sh \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/nekRS \
+    -DENABLE_HYPRE_GPU=on \
+    -DNEKRS_Fortran_FLAGS="-fuse-ld=bfd" \
+    -DNEKRS_GPU_MPI=ON \
+    2>&1 | tee $INSTALL_DIR/setup/log.build
+
+echo "++++++++++++++++++++++"
+echo "++++ NekRS Built +++++"
+echo "++++++++++++++++++++++"
